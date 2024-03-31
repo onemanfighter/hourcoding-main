25af311eae6ff2f14844df2c63487eb8a730ab52
Amit Raikwar <managerarbusiness@gmail.com>
2024-03-31 14:22:57 +0530

    Daisy UI fixes Adding framer motion dependency

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 function App() {
   return (
     <React.StrictMode>
-      <div>
-        <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-          Button
-        </button>
-      </div>
+      <div>App</div>
     </React.StrictMode>
   );
 }
