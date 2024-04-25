533e6f2e25bb4ddc75c3f3382dcf39b001b540a8
One Man Fighter <149316922+onemanfighter@users.noreply.github.com>
2024-04-26 04:56:33 +0530

    Setup (#17)
    * [Zustand]: basic store setup done
    
    * [Jest and testing setup]
    
    * [Cypress setup]
    
    * [i18next] setup
    
    * [Craco setup]

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import './localization/config';
 
 test('renders learn react link', () => {
   render(<App />);
+
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(screen.getByText('App made by Amit')).toBeDefined();
 });
