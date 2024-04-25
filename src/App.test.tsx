e3ff1195fd2a53f730da9f6fbecad48a55547735
Amit Raikwar <managerarbusiness@gmail.com>
2024-04-26 04:21:48 +0530

    [Jest and testing setup]

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+
+  expect(screen.getByText('App')).toBeDefined();
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
